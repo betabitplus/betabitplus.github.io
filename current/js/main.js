45ee73e42e3d8db1ea17ff9533637c736996d4e9
betabitplus <betabitpro@gmail.com>
2015-08-20 18:17:15 +0300

    demo

---
+++ current/js/main.js
@@ -0,0 +1,7 @@
+// var waypoint = new Waypoint({
+//   element: document.getElementById('sidebar'),
+//   handler: function() {
+//     this.element.style.position = "fixed";
+//     this.element.style.top = 0;
+//   }
+// })
