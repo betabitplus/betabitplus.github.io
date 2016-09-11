9be542f9961b38b8ccfb64fc253f543475b77fb4
betabitplus <betabitpro@gmail.com>
2016-09-11 15:50:32 +0300

    change2

--- braveNewWorld/main.js
+++ braveNewWorld/main.js
@@ -1,23 +1,25 @@
-let s = "&nbsp;&nbsp;";
-let b = "<br>";
-let B = `******${b}**${s + s + s}**${b}**${s + s + s}**${b}******${b}**${s + s + s}**${b}**${s + s + s}**${b}******`;
-let R = `******${b}**${s + s + s}**${b}**${s + s + s}**${b}******${b}**${s}**${b}**${s + s}**${b}**${s + s + s}**`;
+"use strict";
 
-let randomize = (letter) => {
-  let target = "*";
-  let pos = 0;
-  let newLetter = "";
+var s = "&nbsp;&nbsp;";
+var b = "<br>";
+var B = "******" + b + "**" + (s + s + s) + "**" + b + "**" + (s + s + s) + "**" + b + "******" + b + "**" + (s + s + s) + "**" + b + "**" + (s + s + s) + "**" + b + "******";
+var R = "******" + b + "**" + (s + s + s) + "**" + b + "**" + (s + s + s) + "**" + b + "******" + b + "**" + s + "**" + b + "**" + (s + s) + "**" + b + "**" + (s + s + s) + "**";
+
+var randomize = function randomize(letter) {
+  var target = "*";
+  var pos = 0;
+  var newLetter = "";
   while (true) {
-    let foundPos = letter.indexOf(target, pos);
+    var foundPos = letter.indexOf(target, pos);
     if (foundPos == -1) break;
 
-    newLetter += letter.slice(pos, foundPos) + `${Math.random() > .5 ? 0 : 1}` + letter.slice(foundPos, 0);
+    newLetter += letter.slice(pos, foundPos) + ("" + (Math.random() > .5 ? 0 : 1)) + letter.slice(foundPos, 0);
     pos = foundPos + 1;
   }
   return newLetter;
-}
+};
 
-setInterval(() => {
+setInterval(function () {
   document.querySelector('.b').innerHTML = randomize(B);
-},200);
+}, 200);
 document.querySelector('.r').innerHTML = R;
